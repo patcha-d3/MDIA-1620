30eb2e1d4fe30c14b6032a25f01a530980af5a74
patcha-d3 <psricome@my.bcit.ca>
2024-09-27 15:20:59 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -11,3 +11,25 @@ let isHorseInside = true;
 console.log(isHorseInside);
 let horseIsNotInside = true;
 console.log(horseIsNotInside);
+
+
+
+
+
+
+// LESSION-4 CODE //
+console.log(typeof 6677)
+console.log(typeof true)
+const STABLE_MONTHLY_FEE = 1000;
+console.log(STABLE_MONTHLY_FEE);
+const lowercasevariable = 199;
+console.log(lowercasevariable);
+console.log(STABLE_MONTHLY_FEE*lowercasevariable*2);
+console.log((40+20)/6);
+console.log("Hi" + " there"); // computer doesn't know how to leave a space, you need to put in the sting
+console.log("Hi3" + 3 );    // type coercion // it's gonna be string
+console.log(false+true+"NaN");  
+console.log("The stable monthly fee is...  " + STABLE_MONTHLY_FEE);
+
+console.log(`My horse's name is "${horseName}" and costs ${STABLE_MONTHLY_FEE} to board him`); // if you use backtick
+console.log("My horse's name is " + horseName + " and he is " + horseAge + " years old now.");
