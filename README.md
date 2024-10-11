a38bb7e84ccdb21783fe23279453d3ce30374527
patcha-d3 <psricome@my.bcit.ca>
2024-10-11 15:59:57 -0700

    Update lesson-6

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
 # MDIA-1620
-**3 AHA Moments**
-- String - data type is a sequence of characters, quotation marks needed, for example: let horseName = "BlahBlah";
-- Number - is a numerical data, type-no quotations needed, for example: let horseAge = 999;
-- Boolean - logical data type, can only be true or false, prefix is "is", "can", "has", for example: let isHorseInside = true;
+**3 Confusing Things**
+- Because I put console.log after let, that's why my houses' locations didn't show up. (now i got it)
+- I always mixed up what to put in the bracket, like (`string`) or (${xxx}) or ("string")
+- ("Hi there" + horsename) and (`Hi there + ${horsename}`) are the same thing
 ---
 
 
-**2 Things that are still unclear**
-- When I tried to edit horseName to other name and saved, VSCode just got mad and showed the ReferenceError
-- What does 'boolean' do? or it just to check true or false?
+**3 Things you have a solid understand of**
+- ===		uses in comparison, but more strict and safer than ==
+- Arrays are zero-indexed; the first item in an array is at index 0.
 
 
 ---
-**1 Word for how you're feeling:**
-- Feeling like a newbie programmer, I learned how to publish my codes to GitHub by not uploading on GitHub website.
-
+**Analogies that you use + gif**
+- ("Hi there" + horsename) and (`Hi there + ${horsename}`) are the same thing
+!(https://media.giphy.com/media/qglTBZFcRu06omNZkA/giphy.gif?cid=ecf05e47oz1affc6r8n26bj8tj9zpmtnrkd2er9qpdgb3yb6&ep=v1_gifs_search&rid=giphy.gif&ct=g)
 
 ---
-**Note**
-git status - shows the status of your files
-git add *filename* - to add file for uploading
-git push - to upload files on GitHub
--- stables.js
+++ stables.js
@@ -8,7 +8,10 @@ console.log(horseAge);
 
 // Boolean - logical data type, can only be true or false, prefix is "is", "can", "has" //
 let isHorseInside = true;
-console.log(isHorseInside);
+
+let areHorsesInside = true;
+
+
 let horseIsNotInside = true;
 console.log(horseIsNotInside);
 
@@ -60,4 +63,61 @@ else {
 }
 
 
+// LESSON-6 //
 
+let word = "Unicorn";
+    console.log(word.charAt(0));
+// answer U
+
+    console.log(word.charAt(1));
+// answer n
+
+let horseNames = [horseName, "Strawberry", "Beans"];
+let horseAges = [horseAge, 2, 10];
+let horseLocations = [areHorsesInside];
+    console.log(horseNames);
+    console.log(horseNames.length);
+    horseNames.push("Lettuce"); // computer will add Lettuce to the horseNames list and remember it
+    console.log(horseNames);
+    console.log(horseNames.length);
+    horseNames.push("Bambie");
+    console.log(horseNames);
+    console.log(horseNames.length);
+    console.log(horseNames[3]);
+    console.log(horseAges[2]);
+    console.log(horseLocations[2]);
+    console.log(`Welcome to my stables! There are ${horseNames.length} horses are staying here`); // or either 
+    console.log(" Welcome to my stables! There are " + horseNames.length + " horses are staying here"); // as well
+   
+   
+   if(horseLocations[1]) {
+        console.log(horseNames[1] + "is outside!");
+   }
+
+    if(horseNames[0] === "Charlie") {
+        console.log("Hi Charlie!");
+    }
+
+    if(horseNames.length < 7) {
+        console.log("These stables are unpopular");
+    }
+
+
+    // LAB 6
+    if(horseLocations[1]) {
+        console.log(horseNames[1] + "are inside the stable!");
+   }
+
+    console.log(`Hi there! ${horseNames}, welcome to the unpopular stables because there are only ${horseNames.length} horses here. `);
+
+// let isHorseInside = true;
+// let areHorseInside = true;
+
+    if(isHorseInside) {
+        console.log(`my horses ${horseName} is inside`)
+    }
+
+    if(areHorsesInside) {
+        console.log(`my horses ${horseNames} are inside `)
+        console.log
+    }
